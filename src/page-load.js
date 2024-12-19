889ddd79d5277531d09b93996f757581c2aa01b3
Darron Dai <daidarron@gmail.com>
2024-12-18 16:01:24 -0800

    Add basic html elements with javascript

---
+++ src/page-load.js
@@ -0,0 +1,18 @@
+// initial page-layout setup module
+import restaurantImg from "./assets/restaurant.jpg";
+
+export function initialPageSetup() {
+  const contentDiv = document.querySelector("div#content");
+
+  const img = document.createElement("img");
+  img.src = restaurantImg;
+  contentDiv.appendChild(img);
+
+  const headline = document.createElement("h1");
+  headline.textContent = "Welcome to our Fancy Restaurant!";
+  contentDiv.appendChild(headline);
+
+  const description = document.createElement("p");
+  description.textContent = "blah blah blah this is description text";
+  contentDiv.appendChild(description);
+}
