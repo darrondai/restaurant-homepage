d598670702739894a13fe668a8e2ab41dd7203d0
Darron Dai <daidarron@gmail.com>
2024-12-18 16:53:38 -0800

    Add todo commments to menu and about page scripts

---
+++ src/aboutPage.js
@@ -0,0 +1,7 @@
+export function aboutPageSetup() {
+  const contentDiv = document.querySelector("div#content");
+  // about header
+  // mission statement
+  // our team
+  // profile pic, name, position
+}
---
+++ src/menuPage.js
@@ -0,0 +1,7 @@
+export function menuPageSetup() {
+  const contentDiv = document.querySelector("div#content");
+
+  // menu header
+  // menuItemList
+  // each item has a img, name, price, ingredients
+}
