5eb4d874cd141bfa9d51911b7f32294f3e9971fa
Darron Dai <daidarron@gmail.com>
2025-02-14 12:43:22 -0800

    Add menu page setup func

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { aboutPageSetup } from "./aboutPage.js";
+import { menuPageSetup } from "./menuPage.js";
 import { homePageSetup } from "./homePage.js";
 
 homePageSetup();
@@ -7,6 +8,9 @@ homePageSetup();
 // when button clicked, clear content and fill with other tab
 const homeBtn = document.querySelector("#home-btn");
 homeBtn.addEventListener("click", homePageSetup);
+
+const menuBtn = document.querySelector("#menu-btn");
+menuBtn.addEventListener("click", menuPageSetup);
 
 const aboutBtn = document.querySelector("#about-btn");
 aboutBtn.addEventListener("click", aboutPageSetup);
--- src/menuPage.js
+++ src/menuPage.js
@@ -1,7 +1,17 @@
 export function menuPageSetup() {
   const contentDiv = document.querySelector("div#content");
-
+  // clear contentDiv
+  contentDiv.textContent = "";
   // menu header
+  const menuHeader = document.createElement("h1");
+  menuHeader.textContent = "MENU HEADER";
+  contentDiv.append(menuHeader);
   // menuItemList
+  const menuItemList = document.createElement("ul");
+  contentDiv.append(menuItemList);
+  // menuItems
   // each item has a img, name, price, ingredients
+  const menuItem = document.createElement("li");
+  menuItem.textContent = "THIS IS A MENU ITEM (CHICKEN PARM)";
+  menuItemList.appendChild(menuItem);
 }
