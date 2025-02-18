97590eb0cfc697dbb0e7ad0247784a9ac6a208cd
Darron Dai <daidarron@gmail.com>
2025-02-17 19:30:38 -0800

    Convert to webpack-merge config, add dev and prod configs

---
+++ webpack.common.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      //   title: "Production",
+      template: "./src/template.html",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
